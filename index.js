bf72bca7d4ec03b1c01a209d0798ab66d85a52f3
Mohammed-Ashraful <mr.ashraful2020@gmail.com>
2021-11-19 17:59:05 +0600

    t

--- index.js
+++ index.js
@@ -40,128 +40,126 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // }
 
 
-// async function run() {
-//   try {
-//     client.connect();
-//     //DATABASE Or Database Collections 
-//     const database = client.db('car_shop');
-//     const userCollection = database.collection('users');
-//     const ordersCollection = database.collection('orders');
-//     const extraCareCollection = database.collection('extra_care');
-//     const carCollection = database.collection('car')
-//     const reviewCollection = database.collection('review')
+async function run() {
+  try {
+    client.connect();
+    //DATABASE Or Database Collections 
+    const database = client.db('car_shop');
+    const userCollection = database.collection('users');
+    const ordersCollection = database.collection('orders');
+    const extraCareCollection = database.collection('extra_care');
+    const carCollection = database.collection('car')
+    const reviewCollection = database.collection('review')
 
 
-//     //Post all orders data to Database
-//     app.post('/car', async (req, res) => {
-//       const orders = req.body;
-//       const result = await carCollection.insertOne(orders);
-//       res.json(result)
-//     })
-//     //  GET API  Load Car  data
-//     app.get('/car', async (req, res) => {
-//       const cursor = carCollection.find({})
-//       const car = await cursor.toArray();
-//       res.send(car)
-//     })
+    //Post all orders data to Database
+    app.post('/car', async (req, res) => {
+      const orders = req.body;
+      const result = await carCollection.insertOne(orders);
+      res.json(result)
+    })
+    //  GET API  Load Car  data
+    app.get('/car', async (req, res) => {
+      const cursor = carCollection.find({})
+      const car = await cursor.toArray();
+      res.send(car)
+    })
 
-//     // GET Single data
-//     app.get('/car/:id', async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: ObjectId(id) }
-//       const cursor = await carCollection.findOne(query);
-//       res.json(cursor)
-//     })
+    // GET Single data
+    app.get('/car/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const cursor = await carCollection.findOne(query);
+      res.json(cursor)
+    })
 
-//     //GET API  Load Car  data in ui
-//     app.get('/', verifyToken , async (req, res) => {
-//       const cursor = extraCareCollection.find({})
-//       const care = await cursor.toArray();
-//       res.send(care)
-//     })
+    //GET API  Load Car  data in ui
+    app.get('/', verifyToken , async (req, res) => {
+      const cursor = extraCareCollection.find({})
+      const care = await cursor.toArray();
+      res.send(care)
+    })
 
-//     //Post all orders data to Database
-//     app.post('/orders', async (req, res) => {
-//       const orders = req.body;
-//       const result = await ordersCollection.insertOne(orders);
-//       res.json(result)
-//     })
+    //Post all orders data to Database
+    app.post('/orders', async (req, res) => {
+      const orders = req.body;
+      const result = await ordersCollection.insertOne(orders);
+      res.json(result)
+    })
 
-//     //get orders data from server
-//     app.get('/orders', async (req, res) => {
-//       const cursor = ordersCollection.find({});
-//       const orders = await cursor.toArray();
-//       res.json(orders)
-//     })
+    //get orders data from server
+    app.get('/orders', async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.json(orders)
+    })
 
-//     //Post user info in server
-//     app.post('/users', async (req, res) => {
-//       const user = req.body;
-//       const result = await userCollection.insertOne(user);
-//       res.json(result)
-//     })
+    //Post user info in server
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.json(result)
+    })
     
-//     app.get('/users', async (req, res) => {
-//       const cursor = userCollection.find({});
-//       const user = await cursor.toArray();
-//       res.json(user)
-//     })
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find({});
+      const user = await cursor.toArray();
+      res.json(user)
+    })
     
     
-//     app.get('/users/:email', async (req, res) => {
-//       const email = req.params.email;
-//       const query = { email };
-//       const user = await userCollection.findOne(query);
-//       let isAdmin = false;
-//       if (user?.role === 'admin') {
-//         isAdmin = true;
-//       }
-//       res.json({ admin: isAdmin })
-//     })
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin })
+    })
 
 
-//     //Post user info in server
-//     app.post('/review', async (req, res) => {
-//       const user = req.body;
-//       const result = await reviewCollection.insertOne(user);
-//       res.json(result)
-//     })
+    //Post user info in server
+    app.post('/review', async (req, res) => {
+      const user = req.body;
+      const result = await reviewCollection.insertOne(user);
+      res.json(result)
+    })
 
-//     app.get('/review', async (req, res) => {
-//       const cursor = reviewCollection.find({});
-//       const user = await cursor.toArray();
-//       res.json(user)
-//     })
-//     //Post user info in server
+    app.get('/review', async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const user = await cursor.toArray();
+      res.json(user)
+    })
+    //Post user info in server
     
 
 
-//     //Create admin
-//     app.put('/users/admin', verifyToken, async (req, res) => {
-//       const user = req.body;
-//       console.log(user);
-//       const requester = req.decodedEmail;
-//       console.log(requester);
-//       if (requester) {
-//         const requesterAccount = await userCollection.findOne({ email: requester });
-//         if (requesterAccount.role === 'admin') {
-//           const filter = { email: user.email };
-//           const updateUser = { $set: { role: "admin" } };
-//           const result = await userCollection.updateOne(filter, updateUser);
-//           res.json(result)
-//         }
-//       }
-//       else {
-//         res.status(403).json({ message: "You don't have access" })
-//       }
-//     })
-//   }
+    //Create admin
+    app.put('/users/admin', verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+        const requesterAccount = await userCollection.findOne({ email: requester });
+        if (requesterAccount.role === 'admin') {
+          const filter = { email: user.email };
+          const updateUser = { $set: { role: "admin" } };
+          const result = await userCollection.updateOne(filter, updateUser);
+          res.json(result)
+        }
+      }
+      else {
+        res.status(403).json({ message: "You don't have access" })
+      }
+    })
+  }
 
-//   catch {
-//     // client.close();
-//   }
-// }
-// run().catch(console.dir)
+  catch {
+    // client.close();
+  }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello !')
